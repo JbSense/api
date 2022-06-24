244a751425b0af20f53f930ecdbb6cf62fa4b644
JbSense <dev.jvxavier@gmail.com>
2022-06-24 19:20:51 -0300

    Commit

--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -1,6 +1,28 @@
 import Admin from '../models/Admin.js'
 
 class AdminController {
+  async login (data) {
+    const email = data.email
+    const password = data.password
+
+    const admin = await Admin.findOne({ where: { email } })
+    console.log(admin.password)
+
+    if (password === admin.password) {
+      return {
+        message: 'Ok',
+        data: {
+          login: true
+        }
+      }
+    }
+
+    return {
+      message: 'Ok',
+      login: false
+    }
+  }
+
   async getAdmin (data) {
     const idAdmin = data.id_admin
     const admin = await Admin.findOne({ where: { id_admin: idAdmin } })
